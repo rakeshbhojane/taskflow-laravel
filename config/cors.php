3795f078913111c07f33c4aad38609746dc0b9ce
rakeshbhojane <rakeshbhojane@gmail.com>
2026-05-18 15:05:59 +0530

    Fix cors.php syntax error

--- config/cors.php
+++ config/cors.php
@@ -3,14 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    ''allowed_origins' => ['*'],' => [
-        'http://localhost:3000',
-        'http://localhost:5173',
-        env('FRONTEND_URL', 'http://localhost:3000'),
-    ],
-    ''allowed_origins' => ['*'],_patterns' => [],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
